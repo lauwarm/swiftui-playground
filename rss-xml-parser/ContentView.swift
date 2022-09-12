0a8fc4a3f0b8953857067abfa92d7c7505b1af7c
lauwarm <10065855+lauwarm@users.noreply.github.com>
2022-09-12 21:31:39 +0200

    Initial Commit

---
+++ rss-xml-parser/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  rss-xml-parser
+//
+//  Created by Fabian on 12.09.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ rss-xml-parser/rss_xml_parserApp.swift
@@ -0,0 +1,17 @@
+//
+//  rss_xml_parserApp.swift
+//  rss-xml-parser
+//
+//  Created by Fabian on 12.09.22.
+//
+
+import SwiftUI
+
+@main
+struct rss_xml_parserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
