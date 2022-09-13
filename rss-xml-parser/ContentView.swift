d8d71be16b8b14e62536c00122604fdc5688a5b2
lauwarm <10065855+lauwarm@users.noreply.github.com>
2022-09-13 11:28:26 +0200

    show rss feed items

--- rss-xml-parser/ContentView.swift
+++ rss-xml-parser/ContentView.swift
@@ -6,12 +6,105 @@
 //
 
 import SwiftUI
+import SWXMLHash
+
+//Identifiable class for List
+class Item: Identifiable
+{
+    var title = ""
+    var url = ""
+    var pubDate = Date()
+}
 
 struct ContentView: View {
+    
+    @State var channelName = ""
+    @State var channelURL = ""
+    @State var imageURL = ""
+    @State var newsItems = [Item]()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            //Channel Information
+            Text("\(channelName)")
+                .font(.system(size: 16))
+                .bold()
+            //Text("\(channelURL)")
+            
+            //This is new in iOS 15 to load an image from a URL
+            AsyncImage(url: URL(string: imageURL))
+                .frame(height: 40)
+            
+            //List of News Items
+            List(newsItems){ item in
+                VStack(alignment: .leading){
+                    Text("\(item.title)")
+                        .bold()
+                    Text(displayDate(date: item.pubDate))
+                        .italic()
+                        .font(.system(size: 14))
+                }
+                .onTapGesture {
+                    UIApplication.shared.open(URL(string: item.url)!)
+                }
+            }
+        }.onAppear(){
+            //Function to retrieve and parse XML data
+            loadData()
+        }
     }
+    
+    func loadData(){
+        let url = NSURL(string: "https://rss.nytimes.com/services/xml/rss/nyt/Europe.xml")
+        
+        let urlArray = [NSURL(string: "https://rss.nytimes.com/services/xml/rss/nyt/Europe.xml"), NSURL(string: "https://rss.nytimes.com/services/xml/rss/nyt/World.xml"), NSURL(string: "https://www.tagesschau.de/xml/rss2/")]
+        
+        urlArray.forEach { test in
+
+        
+        let task = URLSession.shared.dataTask(with: test! as URL) {(data, response, error) in
+                if data != nil
+                {
+                    let feed=NSString(data: data!, encoding: String.Encoding.utf8.rawValue)! as String
+                    let xml = XMLHash.parse(feed)
+                    
+                    channelName = xml["rss"]["channel"]["title"].element!.text
+                    channelURL = xml["rss"]["channel"]["link"].element!.text
+                    //imageURL = xml["rss"]["channel"]["image"]["url"].element!.text
+
+                    for elem in xml["rss"]["channel"]["item"].all
+                    {
+                        let item = Item()
+                        item.title = elem["title"].element!.text
+                        item.url = elem["link"].element!.text
+                        item.pubDate = cleanDate(date: elem["pubDate"].element!.text)
+                        newsItems.append(item)
+                        
+                        //Sort the news items by publication date
+                        newsItems = newsItems.sorted{$0.pubDate.compare($1.pubDate) == .orderedDescending}
+                    }
+                }
+            }
+            
+            task.resume()
+        }
+    }
+    
+    func cleanDate(date: String) -> Date
+    {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "E, d MMM yyyy HH:mm:ss Z"
+        
+        return dateFormatter.date(from:date)!
+    }
+    
+    func displayDate(date: Date) -> String
+    {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE, MMM d, yyyy HH:mm"
+        return dateFormatter.string(from:date)
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
